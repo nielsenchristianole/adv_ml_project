Fix ensemble model energy calculation

geodesics/curve_fitter.py
@@ -1,3 +1,4 @@
+import pdb
 from abc import ABC, abstractmethod, abstractproperty
 from copy import deepcopy
 from dataclasses import dataclass, field
@@ -29,7 +30,7 @@ class OptimizerClass(Enum):
 class CurveConfig(nn.Module):
     num_points: int
     emb_dim: int
-    
+
     # In case of a polynomial curve
     curve_degree: Optional[int] = None
     polynomial_weights: Optional[torch.Tensor] = None
@@ -94,7 +95,17 @@ class CurveFitter(nn.Module, ABC):
         return energy
     
     def kl_energy(self, points: torch.Tensor) -> torch.Tensor:
-        energy = sum(KL(p, q) for p, q in zip(points[:-1], points[1:])) # eq: 12.26
+
+        # if it is an ensemble model
+        if isinstance(points[0], list):
+            #    def sample_energy(self, z, num_montecarlo_samples=100) -> torch.Tensor:
+            divergence = 0
+            for ps, qs in zip(points[:-1], points[1:]):
+                for p, q in zip(ps,qs):
+                    divergence += KL(p, q)
+            energy = divergence / len(points[0])
+        else:
+            energy = sum(KL(p, q) for p, q in zip(points[:-1], points[1:])) # eq: 12.26
         return energy
     
     def infer_energy(self, points: torch.Tensor) -> torch.Tensor:
@@ -220,7 +231,7 @@ class PiecewiseCurveFitter(CurveFitter):
     def get_intermidiate_points(self) -> torch.Tensor:
         t = torch.linspace(0, 1, self.config.num_points, device=self.device)[1:-1, None]
         intermidiate_points = (1 - t) * self.start_point[None, :] + t * self.end_point[None, :]
-        # intermidiate_points += torch.randn_like(intermidiate_points) * 1e-1 # Makes no difference
+        #intermidiate_points += torch.randn_like(intermidiate_points) * 1e-1 # Makes no difference
         return torch.nn.Parameter(intermidiate_points, requires_grad=True)
 
     @property

geodesics/project2.py
@@ -192,13 +192,13 @@ class VAEENSEMBLE(nn.Module):
         decoder, = self.sample_decoder()
         return decoder(z).sample()
 
-    def sample_energy(self, z, num_montecarlo_samples=100) -> torch.Tensor:
-        divergense = 0
-        for z_i, z_j in zip(z[:-1], z[1:]):
-            for _ in range(num_montecarlo_samples):
-                decoder_1, decoder_2 = self.sample_decoder(2)
-                divergense += KL(decoder_1(z_i), decoder_2(z_j))
-        return divergense / num_montecarlo_samples
+    # def sample_energy(self, z, num_montecarlo_samples=100) -> torch.Tensor:
+    #     divergense = 0
+    #     for z_i, z_j in zip(z[:-1], z[1:]):
+    #         for _ in range(num_montecarlo_samples):
+    #             decoder_1, decoder_2 = self.sample_decoder(2) # gets two random decoders from the list of 10
+    #             divergense += KL(decoder_1(z_i), decoder_2(z_j))
+    #     return divergense / num_montecarlo_samples
 
     def fewer_decoders(self, num_decoders=10) -> 'VAEENSEMBLE':
         this = deepcopy(self)
@@ -303,7 +303,7 @@ def main():
 
     from torchvision import datasets, transforms
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default='part-a', choices=['train', 'plot', 'part-a', 'train-ensemble', 'part-b'], help='what to do when running the script (default: %(default)s)')
+    parser.add_argument('--mode', type=str, default='part-b', choices=['train', 'plot', 'part-a', 'train-ensemble', 'part-b'], help='what to do when running the script (default: %(default)s)')
     parser.add_argument('--model', type=str, default='../assets/model.pt', help='file to save model to or load model from (default: %(default)s)')
     parser.add_argument('--plot-dir', type=str, default='../assets/', help='file to save latent plot in (default: %(default)s)')
     parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu', choices=['cpu', 'cuda', 'mps'], help='torch device (default: %(default)s)')
@@ -382,7 +382,7 @@ def main():
         # Save model
         torch.save(model.state_dict(), args.model)
 
-###### we did the rest of this file.
+    ###### we did the rest of this file.
     
     elif args.mode == 'train-ensemble':
         num_ensemble = 10
@@ -490,9 +490,14 @@ def main():
                 curve_indices = torch.randint(num_train_data, (num_curves, 2)) # (num_curves) x 2
               
             curve_config.optimizer_kwargs=dict(epochs=200, lr=1e-1)
-            curve_config.curve_to_energy = Curve2Energy.PASS # TODO: try with other optimizer
-            curve_config.optimizer_class = OptimizerClass.ADAM
-            curve_config.decoder = lambda z: model.sample_energy(z, num_montecarlo_samples)
+            # curve_config.optimizer_kwargs = dict(lr=1*0.2, max_iter=5*100000, line_search_fn='strong_wolfe')
+            curve_config.curve_to_energy = Curve2Energy.KL #! Need PASS because we are using the ensemble model
+            curve_config.optimizer_class = OptimizerClass.ADAM # ? Try m
+            # curve_config.decoder = lambda z: model.sample_energy(z, num_montecarlo_samples)
+            # curve_config.decoder = lambda z: model.decode(z)
+            curve_config.decoder = lambda z: [
+                [dec(z_i) for dec in model.sample_decoder(num_montecarlo_samples)] for z_i in z
+            ]
 
             curve_fitter = curve_fitter_class(curve_config, device=device, verbose_energies=verbose_energies)
 